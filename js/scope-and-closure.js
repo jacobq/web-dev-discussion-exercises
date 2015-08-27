b260a605b5966e40d9c945915218e20600b94850
Jacob <jacobq@gmail.com>
2015-08-26 23:51:06 -0500

    Created placeholder for scope-and-closures.js

---
+++ js/scope-and-closure.js
@@ -0,0 +1,5 @@
+// TODO: Create example of how scope and closures work
+// Perhaps draw inspiration from MDN
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+// or Kyle Simpson's book: 
+// https://github.com/getify/You-Dont-Know-JS/tree/master/scope%20%26%20closures
